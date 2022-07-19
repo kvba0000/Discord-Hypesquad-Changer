3005b6c9a5e347f7de609097b6f6211efc3d2eee
rurq <47297843+kingrurq@users.noreply.github.com>
2022-07-19 18:58:21 +0200

    🤓

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Discord Hypesquad Changer
- 
+Script for discord that allows you to change your hypesquad house and badge assigned to it.
+
+### Dependencies
+- [Node.js (tested on v16.15.1)](https://nodejs.org/en/download/)
+- npm (can be any other package manager like yarn but then you need to install it manually)
+
+### Instalation
+Just simply run file `install.bat` or if you want to do it manually go to `src` folder and run command:
+```
+npm i
+```
+### Usage
+Put your discord token into `config.txt` file, run `start.bat`, (alternatively you can run one of commands below from `src` folder:)
+```
+node .
+or
+npm start
+```
+and then just pick house you want change to.
+### Hypesquad Houses:
+| Preview | Name |
+| ------- | ---- |
+| <p align=center><img src="https://static.wikia.nocookie.net/discord/images/b/b5/Bravery.png" width=30 /></p> | Bravery |
+| <p align=center><img src="https://static.wikia.nocookie.net/discord/images/b/b8/Brilliance.png" width=30 /></p> | Brilliance |
+| <p align=center><img src="https://static.wikia.nocookie.net/discord/images/5/50/Balance.png" width=30 /></p> | Balance |
+### Forks
+If you want to fork it then there's no problems, i'll probably not gonna update it so often because it doesn't have so many functionalities that might break within time.
+And if you think it's a virus then that's why it is on github, you can check code whenever you want!
+### Preview
+![prev1](https://user-images.githubusercontent.com/47297843/179804590-17168596-9533-4fa0-a934-be04f2fc9be8.png)
+![prev2](https://user-images.githubusercontent.com/47297843/179804629-69959173-2661-426b-ae19-c798f3c595db.png)
+![prev3](https://user-images.githubusercontent.com/47297843/179804690-8afd4180-4f67-42cf-aadb-859fb0b19f08.png)
---
+++ src/app.js
@@ -0,0 +1,93 @@
+import axios from "axios";
+import { readFileSync } from "node:fs";
+import {createInterface} from "node:readline";
+(async()=>{
+    console.clear();
+    process.title = "why am i even putting title to this"
+
+    var config = JSON.parse(readFileSync("../config.json").toString()), // config
+        rl = createInterface(process.stdin, process.stdout), // readline interface
+        /**
+         * Sends a question to the user and returns the answer.
+         * @param {string} question - question
+         * @returns {Promise<string>} - answer
+         */
+        q = async (p) => {
+            return new Promise((resolve, reject) => {
+                rl.question(p, (answer) => {
+                    resolve(answer);
+                    rl.pause();
+                });
+            });
+        }
+
+        // checking if the token is valid
+    if(!config.token || new RegExp(/[\w]{24}\.[\w]{6}\.[\w-_]{27}/).test(config.token) === false) {
+        console.log("\x1b[31mNo token provided!\x1b[0m");
+        process.exit(1);
+    }
+
+    var houses = [
+        "Bravery", "Brilliance", "Balance"
+    ]
+
+    //loop until user chooses correct house
+    while(true){
+        console.clear();
+        console.log(`
+        \x1b[34m.########..##.....##.########...#######......##.....##.##....##.########
+        .##.....##.##.....##.##.....##.##.....##......##...##...##..##.......##.
+        .##.....##.##.....##.##.....##.##.....##.......##.##.....####.......##..
+        .########..##.....##.########..##.....##........###.......##.......##...
+        .##...##...##.....##.##...##...##..##.##.......##.##......##......##....
+        .##....##..##.....##.##....##..##....##..###..##...##.....##.....##.....
+        .##.....##..#######..##.....##..#####.##.###.##.....##....##....########
+        Discord Hypesquad Changer
+        Created by: rurq\x1b[0m
+            
+        \x1b[36mPick Hypesquad House:\n${houses.map((h,i) => `       ${i+1}.${h}`).join("\n")}`);
+        
+    var answer = await q("      > ");
+    if(Number(answer) > 0 && Number(answer) < 4){
+        break;
+    }
+    }
+
+    console.log(`\n\nChanging to ${houses[Number(answer)]}...\x1b[0m`);
+    axios.post("https://discord.com/api/v9/hypesquad/online", { // sending request to discord about house change
+        "house_id": answer
+    },{
+        "headers": {
+            "Authorization": config.token,
+        }
+    }).then(r => { // respnonse handler
+        console.log("\n\n\x1b[32mChanged!\x1b[0m");
+        process.exit(0);
+    }).catch(e => { // error handling
+        var error = "";
+        switch(e.response.status){
+            case 400:
+                error = "Bad request error, probably script is outdated, check if you have the latest version of the script, and if yes, wait for it to be updated.";
+                break;
+            case 401:
+                error = "Unauthorized error, probably you have provided an invalid token.";
+                break;
+            case 403:
+                error = "Forbidden error.";
+                break;
+            case 429:
+                var time = e.response.data.retry_after;
+                error = `Too many requests error, try again in a few seconds (${time}), because you got rate limitted.`;
+                break;
+        }
+        console.log(`\n\n\x1b[31mError!\n${error}\x1b[0m`);
+        process.exit(1);
+    })
+    
+
+
+
+
+
+    
+})()
